3935c8f31e9ccd4fbcfa41e94ad9f811cf25c09e
Victor Gavrish <victor.gavrish@gmail.com>
2018-12-16 13:59:51 +0300

    Fix deploy script

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 # install prerquisites
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+source ~/.cargo/env
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install v10.5
