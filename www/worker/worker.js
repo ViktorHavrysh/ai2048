7fc3e63e20dd443e426d92a318b1c040505297c4
Victor Gavrish <victor.gavrish@gmail.com>
2018-12-19 05:14:21 +0300

    Small fixes

--- www/worker/worker.js
+++ www/worker/worker.js
@@ -1,8 +1,8 @@
 import("../../ai2048-wasm/pkg").then(ai2048 => {
   ai2048.init();
-  self.addEventListener("message", async ent => {
+  self.addEventListener("message", ent => {
     const message = ent.data;
-    let mv = ai2048.evaluate_position(
+    const mv = ai2048.evaluate_position(
       message.grid,
       message.minProb,
       message.maxDepth
