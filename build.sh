c393ed6b8b5d68b8b8133d72975d85c056170a70
Victor Gavrish <victor.gavrish@gmail.com>
2019-04-01 02:50:56 +0300

    Update NPM

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 source ~/.cargo/env
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 source ~/.nvm/nvm.sh
-nvm install v10.5
+nvm install v11.4
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -f
 
 # build website
