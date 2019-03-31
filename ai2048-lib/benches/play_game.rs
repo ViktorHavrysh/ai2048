0a727a78a58ccb551348647c977b64888fc6e3d7
Victor Gavrish <victor.gavrish@gmail.com>
2019-03-31 03:17:31 +0300

    Remove silly benchmark

--- ai2048-lib/benches/play_game.rs
+++ ai2048-lib/benches/play_game.rs
@@ -6,20 +6,6 @@ use lazy_static::lazy_static;
 
 const MIN_PROBABILITY: f32 = 0.0001;
 
-fn play_moves(start: Grid, moves: u32) -> SearchResult {
-    let mut grid = start;
-    let mut result = SearchResult::default();
-    for _ in 0..moves {
-        result = searcher::search(grid, MIN_PROBABILITY);
-        if let Some(mv) = result.best_move {
-            grid = grid.make_move(mv).add_random_tile();
-        } else {
-            break;
-        }
-    }
-    result
-}
-
 fn calc_move(start: Grid) -> SearchResult {
     searcher::search(start, MIN_PROBABILITY)
 }
@@ -43,22 +29,10 @@ fn single_moves(c: &mut Criterion) {
     c.bench_function("depth 8 move", move |b| b.iter(|| calc_move(*GRID_DEPTH_8)));
 }
 
-fn multiple_moves(c: &mut Criterion) {
-    c.bench_function("100 moves post depth 8", |b| {
-        b.iter(|| play_moves(*GRID_DEPTH_8, 100))
-    });
-}
-
 criterion_group! {
     name = large_sample;
     config = Criterion::default().sample_size(10);
     targets = single_moves
 }
 
-criterion_group! {
-    name = small_sample;
-    config = Criterion::default().sample_size(3);
-    targets = multiple_moves
-}
-
-criterion_main!(large_sample, small_sample);
+criterion_main!(large_sample);
