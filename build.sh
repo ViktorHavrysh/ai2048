3dda7100024b3d096b06fc46458e6a8e5ff85af9
Victor Gavrish <victor.gavrish@gmail.com>
2019-11-26 15:03:52 +0300

    Update packages

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 # install prerquisites
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source ~/.cargo/env
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
 source ~/.nvm/nvm.sh
-nvm install v11.4
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -f
+nvm install v12.13
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
 # build website
 mkdir -p target && ln -sf ../target ai2048-wasm/target
